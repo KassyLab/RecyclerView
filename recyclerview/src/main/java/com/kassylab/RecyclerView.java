213bd32b5c7a7040188b2530d32b8f44da622090
damien.danglard@outlook.fr <D.damiendu63>
2018-02-22 22:31:48 +0100

    Add choicemode to RecyclerView

--- recyclerview/src/main/java/com/kassylab/RecyclerView.java
+++ recyclerview/src/main/java/com/kassylab/RecyclerView.java
@@ -18,10 +18,18 @@ package com.kassylab;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v4.util.LongSparseArray;
 import android.util.AttributeSet;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SoundEffectConstants;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.AbsListView;
+import android.widget.Checkable;
+import android.widget.ListAdapter;
 
 /**
  * A flexible view for providing a limited window into a large data set.
@@ -92,6 +100,32 @@ import android.view.accessibility.AccessibilityEvent;
 public class RecyclerView extends android.support.v7.widget.RecyclerView {
 	
 	/**
+	 * Represents an invalid position. All valid positions are in the range 0 to 1 less than the
+	 * number of items in the current adapter.
+	 */
+	public static final int INVALID_POSITION = -1;
+	
+	/**
+	 * Normal list that does not indicate choices
+	 */
+	public static final int CHOICE_MODE_NONE = 0;
+	
+	/**
+	 * The list allows up to one choice
+	 */
+	public static final int CHOICE_MODE_SINGLE = 1;
+	
+	/**
+	 * The list allows multiple choices
+	 */
+	public static final int CHOICE_MODE_MULTIPLE = 2;
+	
+	/**
+	 * The list allows multiple choices in a modal selection mode
+	 */
+	public static final int CHOICE_MODE_MULTIPLE_MODAL = 3;
+	
+	/**
 	 * The listener that receives notifications when an item is clicked.
 	 */
 	OnItemClickListener mOnItemClickListener;
@@ -100,6 +134,40 @@ public class RecyclerView extends android.support.v7.widget.RecyclerView {
 	 * The listener that receives notifications when an item is long clicked.
 	 */
 	OnItemLongClickListener mOnItemLongClickListener;
+	
+	/**
+	 * Controls if/how the user may choose/check items in the list
+	 */
+	int mChoiceMode = CHOICE_MODE_NONE;
+	
+	/**
+	 * Controls CHOICE_MODE_MULTIPLE_MODAL. null when inactive.
+	 */
+	ActionMode mChoiceActionMode;
+	
+	/**
+	 * Wrapper for the multiple choice mode callback; AbsListView needs to perform
+	 * a few extra actions around what application code does.
+	 */
+	MultiChoiceModeWrapper mMultiChoiceModeCallback;
+	
+	/**
+	 * Running count of how many items are currently checked
+	 */
+	int mCheckedItemCount;
+	
+	/**
+	 * Running state of which positions are currently checked
+	 */
+	SparseBooleanArray mCheckStates;
+	
+	/**
+	 * Running state of which IDs are currently checked.
+	 * If there is a value for a given key, the checked state for that ID is true
+	 * and the value holds the last known position in the adapter for that id.
+	 */
+	LongSparseArray<Integer> mCheckedIdStates;
+	
 	
 	public RecyclerView(Context context) {
 		super(context);
@@ -152,35 +220,6 @@ public class RecyclerView extends android.support.v7.widget.RecyclerView {
 	}
 	
 	/**
-	 * Call the OnItemClickListener, if it is defined. Performs all normal
-	 * actions associated with clicking: reporting accessibility event, playing
-	 * a sound, etc.
-	 *
-	 * @param holder The view within the AdapterView that was clicked.
-	 * @param position The position of the view in the adapter.
-	 * @param id The row id of the item that was clicked.
-	 * @return True if there was an assigned OnItemClickListener that was
-	 *         called, false otherwise is returned.
-	 */
-	
-	@SuppressWarnings("UnusedReturnValue")
-	public boolean performItemClick(com.kassylab.ViewHolder holder, int position, long id) {
-		final boolean result;
-		if (mOnItemClickListener != null) {
-			playSoundEffect(SoundEffectConstants.CLICK);
-			mOnItemClickListener.onItemClick(this, holder, position, id);
-			result = true;
-		} else {
-			result = false;
-		}
-		
-		if (holder != null) {
-			holder.itemView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
-		}
-		return result;
-	}
-	
-	/**
 	 * Interface definition for a callback to be invoked when an item in this
 	 * view has been clicked and held.
 	 */
@@ -223,4 +262,437 @@ public class RecyclerView extends android.support.v7.widget.RecyclerView {
 	public final OnItemLongClickListener getOnItemLongClickListener() {
 		return mOnItemLongClickListener;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Returns the number of items currently selected. This will only be valid
+	 * if the choice mode is not {@link #CHOICE_MODE_NONE} (default).
+	 *
+	 * <p>To determine the specific items that are currently selected, use one of
+	 * the <code>getChecked*</code> methods.
+	 *
+	 * @return The number of items currently selected
+	 *
+	 * @see #getCheckedItemPosition()
+	 * @see #getCheckedItemPositions()
+	 * @see #getCheckedItemIds()
+	 * @see AbsListView#getCheckedItemCount()
+	 */
+	public int getCheckedItemCount() {
+		return mCheckedItemCount;
+	}
+	
+	/**
+	 * Returns the checked state of the specified position. The result is only
+	 * valid if the choice mode has been set to {@link #CHOICE_MODE_SINGLE}
+	 * or {@link #CHOICE_MODE_MULTIPLE}.
+	 *
+	 * @param position The item whose checked state to return
+	 * @return The item's checked state or <code>false</code> if choice mode
+	 *         is invalid
+	 *
+	 * @see #setChoiceMode(int)
+	 * @see AbsListView#isItemChecked(int)
+	 */
+	public boolean isItemChecked(int position) {
+		return mChoiceMode != CHOICE_MODE_NONE && mCheckStates != null && mCheckStates.get(position);
+		
+	}
+	
+	/**
+	 * Returns the currently checked item. The result is only valid if the choice
+	 * mode has been set to {@link #CHOICE_MODE_SINGLE}.
+	 *
+	 * @return The position of the currently checked item or
+	 *         {@link #INVALID_POSITION} if nothing is selected
+	 *
+	 * @see #setChoiceMode(int)
+	 * @see AbsListView#getCheckedItemPosition()
+	 */
+	public int getCheckedItemPosition() {
+		if (mChoiceMode == CHOICE_MODE_SINGLE && mCheckStates != null && mCheckStates.size() == 1) {
+			return mCheckStates.keyAt(0);
+		}
+		
+		return INVALID_POSITION;
+	}
+	
+	/**
+	 * Returns the set of checked items in the list. The result is only valid if
+	 * the choice mode has not been set to {@link #CHOICE_MODE_NONE}.
+	 *
+	 * @return  A SparseBooleanArray which will return true for each call to
+	 *          get(int position) where position is a checked position in the
+	 *          list and false otherwise, or <code>null</code> if the choice
+	 *          mode is set to {@link #CHOICE_MODE_NONE}.
+	 *
+	 * @see AbsListView#getCheckedItemPositions()
+	 */
+	public SparseBooleanArray getCheckedItemPositions() {
+		if (mChoiceMode != CHOICE_MODE_NONE) {
+			return mCheckStates;
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns the set of checked items ids. The result is only valid if the
+	 * choice mode has not been set to {@link #CHOICE_MODE_NONE} and the adapter
+	 * has stable IDs. ({@link ListAdapter#hasStableIds()} == {@code true})
+	 *
+	 * @return A new array which contains the id of each checked item in the
+	 *         list.
+	 *
+	 * @see AbsListView#getCheckedItemIds()
+	 */
+	public long[] getCheckedItemIds() {
+		if (mChoiceMode == CHOICE_MODE_NONE || mCheckedIdStates == null || getAdapter() == null) {
+			return new long[0];
+		}
+		
+		final LongSparseArray<Integer> idStates = mCheckedIdStates;
+		final int count = idStates.size();
+		final long[] ids = new long[count];
+		
+		for (int i = 0; i < count; i++) {
+			ids[i] = idStates.keyAt(i);
+		}
+		
+		return ids;
+	}
+	
+	/**
+	 * Clear any choices previously set
+	 *
+	 * @see AbsListView#clearChoices()
+	 */
+	public void clearChoices() {
+		if (mCheckStates != null) {
+			mCheckStates.clear();
+		}
+		if (mCheckedIdStates != null) {
+			mCheckedIdStates.clear();
+		}
+		mCheckedItemCount = 0;
+	}
+	
+	/**
+	 * Sets the checked state of the specified position. The is only valid if
+	 * the choice mode has been set to {@link #CHOICE_MODE_SINGLE} or
+	 * {@link #CHOICE_MODE_MULTIPLE}.
+	 *
+	 * @param position The item whose checked state is to be checked
+	 * @param value The new checked state for the item
+	 */
+	public void setItemChecked(int position, boolean value) {
+		if (mChoiceMode == CHOICE_MODE_NONE) {
+			return;
+		}
+		
+		// Start selection mode if needed. We don't need to if we're unchecking something.
+		if (value && mChoiceMode == CHOICE_MODE_MULTIPLE_MODAL && mChoiceActionMode == null) {
+			if (mMultiChoiceModeCallback == null ||
+					!mMultiChoiceModeCallback.hasWrappedCallback()) {
+				throw new IllegalStateException("AbsListView: attempted to start selection mode " +
+						"for CHOICE_MODE_MULTIPLE_MODAL but no choice mode callback was " +
+						"supplied. Call setMultiChoiceModeListener to set a callback.");
+			}
+			mChoiceActionMode = startActionMode(mMultiChoiceModeCallback);
+		}
+		
+		final boolean itemCheckChanged;
+		if (mChoiceMode == CHOICE_MODE_MULTIPLE || mChoiceMode == CHOICE_MODE_MULTIPLE_MODAL) {
+			boolean oldValue = mCheckStates.get(position);
+			mCheckStates.put(position, value);
+			if (mCheckedIdStates != null && getAdapter().hasStableIds()) {
+				if (value) {
+					mCheckedIdStates.put(getAdapter().getItemId(position), position);
+				} else {
+					mCheckedIdStates.delete(getAdapter().getItemId(position));
+				}
+			}
+			itemCheckChanged = oldValue != value;
+			if (itemCheckChanged) {
+				if (value) {
+					mCheckedItemCount++;
+				} else {
+					mCheckedItemCount--;
+				}
+			}
+			if (mChoiceActionMode != null) {
+				final long id = getAdapter().getItemId(position);
+				mMultiChoiceModeCallback.onItemCheckedStateChanged(mChoiceActionMode,
+						position, id, value);
+			}
+		} else {
+			boolean updateIds = mCheckedIdStates != null && getAdapter().hasStableIds();
+			// Clear all values if we're checking something, or unchecking the currently
+			// selected item
+			itemCheckChanged = isItemChecked(position) != value;
+			if (value || isItemChecked(position)) {
+				mCheckStates.clear();
+				if (updateIds) {
+					mCheckedIdStates.clear();
+				}
+			}
+			// this may end up selecting the value we just cleared but this way
+			// we ensure length of mCheckStates is 1, a fact getCheckedItemPosition relies on
+			if (value) {
+				mCheckStates.put(position, true);
+				if (updateIds) {
+					mCheckedIdStates.put(getAdapter().getItemId(position), position);
+				}
+				mCheckedItemCount = 1;
+			} else if (mCheckStates.size() == 0 || !mCheckStates.valueAt(0)) {
+				mCheckedItemCount = 0;
+			}
+		}
+		
+		// Do not generate a data change while we are in the layout phase or data has not changed
+		if (/*!mInLayout && !mBlockLayoutRequests &&*/ itemCheckChanged) {
+			/*mDataChanged = true;
+			rememberSyncState();*/
+			requestLayout();
+		}
+	}
+	
+	/**
+	 * Call the OnItemClickListener, if it is defined. Performs all normal
+	 * actions associated with clicking: reporting accessibility event, playing
+	 * a sound, etc.
+	 *
+	 * @param holder The view within the AdapterView that was clicked.
+	 * @param position The position of the view in the adapter.
+	 * @param id The row id of the item that was clicked.
+	 * @return True if there was an assigned OnItemClickListener that was
+	 *         called, false otherwise is returned.
+	 */
+	@SuppressWarnings("UnusedReturnValue")
+	public boolean performItemClick(com.kassylab.ViewHolder holder, int position, long id) {
+		boolean handled = false;
+		boolean dispatchItemClick = true;
+		
+		if (mChoiceMode != CHOICE_MODE_NONE) {
+			handled = true;
+			boolean checkedStateChanged = false;
+			
+			if (mChoiceMode == CHOICE_MODE_MULTIPLE ||
+					(mChoiceMode == CHOICE_MODE_MULTIPLE_MODAL && mChoiceActionMode != null)) {
+				boolean checked = !mCheckStates.get(position, false);
+				mCheckStates.put(position, checked);
+				if (mCheckedIdStates != null && getAdapter().hasStableIds()) {
+					if (checked) {
+						mCheckedIdStates.put(getAdapter().getItemId(position), position);
+					} else {
+						mCheckedIdStates.delete(getAdapter().getItemId(position));
+					}
+				}
+				if (checked) {
+					mCheckedItemCount++;
+				} else {
+					mCheckedItemCount--;
+				}
+				if (mChoiceActionMode != null) {
+					mMultiChoiceModeCallback.onItemCheckedStateChanged(mChoiceActionMode,
+							position, id, checked);
+					dispatchItemClick = false;
+				}
+				checkedStateChanged = true;
+			} else if (mChoiceMode == CHOICE_MODE_SINGLE) {
+				boolean checked = !mCheckStates.get(position, false);
+				if (checked) {
+					mCheckStates.clear();
+					mCheckStates.put(position, true);
+					if (mCheckedIdStates != null && getAdapter().hasStableIds()) {
+						mCheckedIdStates.clear();
+						mCheckedIdStates.put(getAdapter().getItemId(position), position);
+					}
+					mCheckedItemCount = 1;
+				} else if (mCheckStates.size() == 0 || !mCheckStates.valueAt(0)) {
+					mCheckedItemCount = 0;
+				}
+				checkedStateChanged = true;
+			}
+			
+			if (checkedStateChanged) {
+				updateOnScreenCheckedViews(holder, position);
+			}
+		}
+		
+		if (dispatchItemClick) {
+			handled |= performItemClick2(holder, position, id);
+		}
+		
+		return handled;
+	}
+	
+	private boolean performItemClick2(com.kassylab.ViewHolder holder, int position, long id) {
+		final boolean result;
+		if (mOnItemClickListener != null) {
+			playSoundEffect(SoundEffectConstants.CLICK);
+			mOnItemClickListener.onItemClick(this, holder, position, id);
+			result = true;
+		} else {
+			result = false;
+		}
+		
+		if (holder != null) {
+			holder.itemView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_CLICKED);
+		}
+		return result;
+	}
+	
+	/**
+	 * Perform a quick, in-place update of the checked or activated state
+	 * on all visible item views. This should only be called when a valid
+	 * choice mode is active.
+	 */
+	private void updateOnScreenCheckedViews(com.kassylab.ViewHolder holder, int position) {
+		if (holder.itemView instanceof Checkable) {
+			((Checkable) holder.itemView).setChecked(mCheckStates.get(position));
+		} else if (getContext().getApplicationInfo().targetSdkVersion
+				>= android.os.Build.VERSION_CODES.HONEYCOMB) {
+			holder.itemView.setActivated(mCheckStates.get(position));
+		}
+		
+		/*final int firstPos = mFirstPosition;
+		final int count = getChildCount();
+		final boolean useActivated = getContext().getApplicationInfo().targetSdkVersion
+				>= android.os.Build.VERSION_CODES.HONEYCOMB;
+		for (int i = 0; i < count; i++) {
+			final View child = getChildAt(i);
+			final int position = firstPos + i;
+			
+			if (child instanceof Checkable) {
+				((Checkable) child).setChecked(mCheckStates.get(position));
+			} else if (useActivated) {
+				child.setActivated(mCheckStates.get(position));
+			}
+		}*/
+	}
+	
+	/**
+	 * @see #setChoiceMode(int)
+	 *
+	 * @return The current choice mode
+	 */
+	public int getChoiceMode() {
+		return mChoiceMode;
+	}
+	
+	/**
+	 * Defines the choice behavior for the List. By default, Lists do not have any choice behavior
+	 * ({@link #CHOICE_MODE_NONE}). By setting the choiceMode to {@link #CHOICE_MODE_SINGLE}, the
+	 * List allows up to one item to  be in a chosen state. By setting the choiceMode to
+	 * {@link #CHOICE_MODE_MULTIPLE}, the list allows any number of items to be chosen.
+	 *
+	 * @param choiceMode One of {@link #CHOICE_MODE_NONE}, {@link #CHOICE_MODE_SINGLE}, or
+	 * {@link #CHOICE_MODE_MULTIPLE}
+	 */
+	public void setChoiceMode(int choiceMode) {
+		mChoiceMode = choiceMode;
+		if (mChoiceActionMode != null) {
+			mChoiceActionMode.finish();
+			mChoiceActionMode = null;
+		}
+		if (mChoiceMode != CHOICE_MODE_NONE) {
+			if (mCheckStates == null) {
+				mCheckStates = new SparseBooleanArray(0);
+			}
+			if (mCheckedIdStates == null && getAdapter() != null && getAdapter().hasStableIds()) {
+				mCheckedIdStates = new LongSparseArray<>(0);
+			}
+			// Modal multi-choice mode only has choices when the mode is active. Clear them.
+			if (mChoiceMode == CHOICE_MODE_MULTIPLE_MODAL) {
+				clearChoices();
+				setLongClickable(true);
+			}
+		}
+	}
+	
+	/**
+	 * Set a {@link AbsListView.MultiChoiceModeListener} that will manage the lifecycle of the
+	 * selection {@link ActionMode}. Only used when the choice mode is set to
+	 * {@link #CHOICE_MODE_MULTIPLE_MODAL}.
+	 *
+	 * @param listener Listener that will manage the selection mode
+	 *
+	 * @see #setChoiceMode(int)
+	 */
+	public void setMultiChoiceModeListener(AbsListView.MultiChoiceModeListener listener) {
+		if (mMultiChoiceModeCallback == null) {
+			mMultiChoiceModeCallback = new MultiChoiceModeWrapper();
+		}
+		mMultiChoiceModeCallback.setWrapped(listener);
+	}
+	
+	
+	
+	
+	
+	
+	class MultiChoiceModeWrapper implements AbsListView.MultiChoiceModeListener {
+		private AbsListView.MultiChoiceModeListener mWrapped;
+		
+		void setWrapped(AbsListView.MultiChoiceModeListener wrapped) {
+			mWrapped = wrapped;
+		}
+		
+		boolean hasWrappedCallback() {
+			return mWrapped != null;
+		}
+		
+		@Override
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			if (mWrapped.onCreateActionMode(mode, menu)) {
+				// Initialize checked graphic state?
+				setLongClickable(false);
+				return true;
+			}
+			return false;
+		}
+		
+		@Override
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			return mWrapped.onPrepareActionMode(mode, menu);
+		}
+		
+		@Override
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			return mWrapped.onActionItemClicked(mode, item);
+		}
+		
+		@Override
+		public void onDestroyActionMode(ActionMode mode) {
+			mWrapped.onDestroyActionMode(mode);
+			mChoiceActionMode = null;
+			
+			// Ending selection mode means deselecting everything.
+			clearChoices();
+			
+			/*mDataChanged = true;
+			rememberSyncState();*/
+			requestLayout();
+			
+			setLongClickable(true);
+		}
+		
+		@Override
+		public void onItemCheckedStateChanged(ActionMode mode,
+		                                      int position, long id, boolean checked) {
+			mWrapped.onItemCheckedStateChanged(mode, position, id, checked);
+			
+			// If there are no items selected we no longer need the selection mode.
+			if (getCheckedItemCount() == 0) {
+				mode.finish();
+			}
+		}
+	}
+	
 }
